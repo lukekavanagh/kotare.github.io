dc0132ec118719e3dc567d2bbda0b48c132a6d45
Esther Greatbanks <esthergreatbanks@gmail.com>
2015-06-05 16:44:02 +1200

    added unicolde scroll character things, removed header text function

--- Board/javascript.js
+++ Board/javascript.js
@@ -7,7 +7,6 @@ $(document).ready(function() {
   };
 
   function Bubble (x,y, randId){
-  	this.header ="this is the header";
   	this.content ="this is the content this is the content this is the content this is the contentth this is the content this is the content this is the content this is the contentth this is the content this is the content this is the content this is the contentth this is the content this is the content this is the content this is the contentththis is the content this is the content this is the content this is the contentthis is the contentthis is the contentthis is the contentthis is the contentthis is the contentthis is the contentthis is the contentthis is the contentthis is the content";
   	this.xOffset=x;
   	this.yOffset=y;
@@ -26,50 +25,45 @@ $(document).ready(function() {
     e.stopImmediatePropagation();
   });
 
-
   function renderBubble(bubble) {
   	$('#board').append(
   		"<div class='bubble' id=" + bubble.id + ">" +
-  		"<div class='header'><a class='delete' contenteditable='false'>X</a></div>" +
+  		"<div class='header'> <a class='delete' contenteditable='false'>X </a> </div>" +
   		"<div class='content' contentEditable='true'></div>"+
       "<div class='footer'>" +
-        "<a class='scrollUp' href='#'>up</a>" +
-        "<a class='scrollDown' href='#'>down</a>" +
-      "</div>"
-  +"</div>")
+      "<a class='scrollUp' href='#'> &#9650 </a>" +
+      "<a class='scrollDown' href='#'> &#9660 </a>" +
+      "</div></div>")
 
   	$(".bubble:last ").offset({top: bubble.xOffset, left: bubble.yOffset});
-  	 $('.bubble:last').draggable({
-        handle: ".header"
+  	$('.bubble:last').draggable({
+      handle: ".header"
+    });
+    $('.bubble:last').resizable();
+    $('.bubble:last .content').append(bubble.content);
+
+
+    $(function(){
+      $('.delete')
+      //.button()
+      .click(function(event){
+        event.stopImmediatePropagation();
+        $(this).parent().parent().remove();
       });
-      $('.bubble:last').resizable();
-      $('.bubble:last .header').append(bubble.header);
-      $('.bubble:last .content').append(bubble.content);
-
-
-      $(function(){
-        $('.delete')
-        //.button()
-        .click(function(event){
-          event.stopImmediatePropagation();
-          $(this).parent().parent().remove();
-        });
     });
 
-      $(".scrollUp").bind("click", function(event) {
-          event.preventDefault();
-          var currentBubbleId = ($(this).parent().parent().attr('id'));
-          console.log(currentBubbleId);
-          console.log( $("#" + currentBubbleId).find(".content"));
-          var scrollHeight = $("#" + currentBubbleId).find(".content").scrollTop();
-          $("#" + currentBubbleId).find(".content").scrollTop(scrollHeight - 25);
-      })
+    $(".scrollUp").bind("click", function(event) {
+      event.preventDefault();
+      var currentBubbleId = ($(this).parent().parent().attr('id'));
+      var scrollHeight = $("#" + currentBubbleId).find(".content").scrollTop();
+      $("#" + currentBubbleId).find(".content").scrollTop(scrollHeight - 25);
+    })
 
-      $(".scrollDown").bind("click", function(event) {
-          event.preventDefault();
-          var currentBubbleId = ($(this).parent().parent().attr('id'));
-          var scrollHeight = $("#" + currentBubbleId).find(".content").scrollTop();
-          $("#" + currentBubbleId).find(".content").scrollTop(scrollHeight + 25);
-      });
+    $(".scrollDown").bind("click", function(event) {
+      event.preventDefault();
+      var currentBubbleId = ($(this).parent().parent().attr('id'));
+      var scrollHeight = $("#" + currentBubbleId).find(".content").scrollTop();
+      $("#" + currentBubbleId).find(".content").scrollTop(scrollHeight + 25);
+    });
   };
 });
