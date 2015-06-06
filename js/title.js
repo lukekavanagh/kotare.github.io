13afef4b31d6bda993f681691fbe39eb9c7cf9a8
peterjacobson <peterjacobson.nz@gmail.com>
2015-06-06 21:15:08 +1200

    uncomment title.js

--- js/title.js
+++ js/title.js
@@ -1,5 +1,5 @@
 
 
-// $(document).ready(function(){
-// 	$("#os-phrases > h2").lettering('words').children("span").lettering().children("span").lettering();
-// });
+$(document).ready(function(){
+	$("#os-phrases > h2").lettering('words').children("span").lettering().children("span").lettering();
+});
