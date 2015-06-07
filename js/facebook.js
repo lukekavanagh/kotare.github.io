4b1b19b94b5381f9aaeb1eae2e099a7bbe531a3a
Rich Churcher <rich.churcher@gmail.com>
2015-06-08 08:24:55 +1200

    Login

--- js/facebook.js
+++ js/facebook.js
@@ -2,10 +2,13 @@
 // Button.  See the onlogin handler attached to it in the sample
 // code below.
 function getAccessToken() {
+  console.log("getAccesstoken()");
   FB.getLoginStatus(function(response) {
     if (response.status === 'connected') {
+      console.log(response);
       // Logged into CRUDbrain and Facebook.
       return response.authResponse.accessToken;
+      console.log(response.authResponse);
     } 
 
     return null;      
