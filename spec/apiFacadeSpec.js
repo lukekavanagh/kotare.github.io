dec4629bcd94b7ccb474ec43da65112fb4213420
Esther Greatbanks <esthergreatbanks@gmail.com>
2015-06-07 17:32:52 +1200

    third test passing put

--- spec/apiFacadeSpec.js
+++ spec/apiFacadeSpec.js
@@ -1,17 +1,73 @@
 describe("ApiFacade", function() {
 
   describe("postBoard()", function() {
-    it("returns a new board", function(){
+    it("returns a new board", function() {
       var newBoard = ApiFacade.postBoard();
       expect(newBoard._id.length).toBeGreaterThan(6);
       expect(newBoard.bubbles.length).toEqual(0);
       expect(newBoard.connections.length).toEqual(0);
     })
   })
-  describe("getboard()", function(){
-    it("returns a bloody great board object", function(){
+
+  describe("getboard()", function() {
+    it("returns a bloody great board object", function() {
       var board = ApiFacade.getBoard();
       expect(board.message.length).toBeGreaterThan(6);
     })
   })
+
+  describe("putBoard()", function(data) {
+    it("returns an updated board", function() {
+        var updatedBoard = ApiFacade.putBoard();
+        expect(updatedBoard.message.length).toBeGreaterThan(6);
+      var board = {
+
+            "_id": "55710b225f229a074eb5fddd",
+            "__v": 2,
+            "connections": [{
+            "startBubbleId": "978351yedfg980",
+            "endBubbleId": "9788923edfg932",
+            "_id": "55711c8cbc28c30d52fdec1c"
+          },
+
+          {
+            "startBubbleId": "978wer9io890weur0",
+            "endBubbleId": "klsooiuiosdfsd",
+          }],
+
+            "bubbles": [{
+            "bubbleId": "98723hooih2348",
+            "heading": "idea#291",
+            "content": "help, I'm stuck in a bubble",
+            "_id": "5571116e1578f5ea505fe932",
+            "size": {
+              "top": 91,
+              "left": 87
+            },
+            "location": {
+              "top": 91,
+              "left": 87
+            }
+          },
+
+          {
+            "bubbleId": "98723hooih2348",
+            "heading": "idea#291",
+            "content": "help, I'm stuck in a bubble",
+            "size": {
+              "top": 91,
+              "left": 87
+            },
+
+            "location": {
+              "top": 91,
+              "left": 87
+            }
+          }
+
+        ]
+      }
+    })
+
+    })
 });
