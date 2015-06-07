c6254fe38e05e23b9b895879c035bf994f7e8a71
Esther Greatbanks <esthergreatbanks@gmail.com>
2015-06-07 15:11:43 +1200

    postBoard test complete

--- spec/apiFacadeSpec.js
+++ spec/apiFacadeSpec.js
@@ -1,6 +1,14 @@
-describe("api Facade", function() {
+describe("ApiFacade", function() {
 
-  describe("get board", function(){
+  describe("postBoard()", function() {
+    it("returns a new board", function(){
+      var newBoard = ApiFacade.postBoard();
+      expect(newBoard._id.length).toBeGreaterThan(6);
+      expect(newBoard.bubbles.length).toEqual(0);
+      expect(newBoard.connections.length).toEqual(0);
+    })
+  })
+  describe("getboard()", function(){
     it("returns a bloody great board object", function(){
       var board = ApiFacade.getBoard();
       expect(board.message.length).toBeGreaterThan(6);
