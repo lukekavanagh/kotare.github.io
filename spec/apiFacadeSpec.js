1e151cf223b9aedbe47bc8c9dbdef9787ed4387a
Esther Greatbanks <esthergreatbanks@gmail.com>
2015-06-07 14:49:19 +1200

    refactored first test

--- spec/apiFacadeSpec.js
+++ spec/apiFacadeSpec.js
@@ -2,10 +2,8 @@ describe("api Facade", function() {
 
   describe("get board", function(){
     it("returns a bloody great board object", function(){
-      // var board = ApiFacade.getBoard();
-      // var board = ApiFacade.response;
-      // console.log(board);
-      expect(ApiFacade.getBoard().message.length).toBeGreaterThan(6);
+      var board = ApiFacade.getBoard();
+      expect(board.message.length).toBeGreaterThan(6);
     })
   })
 });
