7e0241613ef33f3927a8cde37481dd4567b1aab1
Rich Churcher <rich.churcher@gmail.com>
2015-06-07 14:19:58 +1200

    Merge resolved

---
+++ js/bubbleView.js
@@ -0,0 +1,58 @@
+function renderBubble(bubble) {
+  $('#board').append(
+    "<div class='bubble' id=" + bubble.bubbleId + ">" +
+    "<div class='header'> <a class='delete' contenteditable='false'>X </a><a class='link'>+</a> </div>" +
+    "<div class='content' contentEditable='true'></div>"+
+    "<div class='footer'>" +
+    "<a class='scrollUp' href='#'> &#9650 </a>" +
+    "<a class='scrollDown' href='#'> &#9660 </a>" +
+    "</div></div>")
+
+  $(".bubble:last ").offset({top: bubble.location.left, left: bubble.location.top});
+  $('.bubble:last').draggable({
+    handle: ".header"
+  });
+  $('.bubble:last').resizable();
+  $('.bubble:last .content').append(bubble.content);
+
+  $(function(){
+    $('.delete')
+    .click(function(event){
+      event.stopImmediatePropagation();
+      $(this).parent().parent().remove();
+    });
+  });
+
+  $('.link').click( function(e) {
+    e.stopImmediatePropagation();
+    if (!connectionInProgress){
+      currentConnection.startBubbleId = $(this).parent().parent().attr('id');
+      connectionInProgress = true;
+    }
+    else {
+      currentConnection.endBubbleId = $(this).parent().parent().attr('id');
+      renderConnections(currentConnection.startBubbleId, currentConnection.endBubbleId, document.mySVG);
+      connectionInProgress = false;
+      board.connections.push({
+        startBubbleId: currentConnection.startBubbleId,
+        endBubbleId: currentConnection.endBubbleId
+      });
+      currentConnection.startBubbleId = "";
+      currentConnection.endBubbleId = "";
+    }
+  });
+
+  $(".scrollUp").bind("click", function(event) {
+    event.preventDefault();
+    var currentBubbleId = ($(this).parent().parent().attr('id'));
+    var scrollHeight = $("#" + currentBubbleId).find(".content").scrollTop();
+    $("#" + currentBubbleId).find(".content").scrollTop(scrollHeight - 25);
+  })
+
+  $(".scrollDown").bind("click", function(event) {
+    event.preventDefault();
+    var currentBubbleId = ($(this).parent().parent().attr('id'));
+    var scrollHeight = $("#" + currentBubbleId).find(".content").scrollTop();
+    $("#" + currentBubbleId).find(".content").scrollTop(scrollHeight + 25);
+  });
+};
