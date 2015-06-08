a837e0b3c02587c3a8ec2c2f3413ea589ee6470e
Rich Churcher <rich.churcher@gmail.com>
2015-06-08 14:44:40 +1200

    Login & logout

--- js/facebook.js
+++ js/facebook.js
@@ -1,17 +1,37 @@
-// This function is called when someone finishes with the Login
-// Button.  See the onlogin handler attached to it in the sample
-// code below.
-function getUser() {
-  console.log("getAccesstoken()");
+var fbUser;
+
+$(function() {
+
+  $('#fbButton').click(function (e) {
+    e.stopImmediatePropagation();
+
+    if (!fbUser) {
+      FB.login(function (response) {
+        setUser(response);
+        $(e.target).toggleClass('logged-in');
+      }, {
+        scope: 'public_profile,email'
+      });  
+    } else {
+      FB.logout(function (response) {
+        $(e.target).toggleClass('logged-in');
+        fbUser = null;
+      });
+    }
+  });
+
+});
+
+function setUser(response) {
   FB.getLoginStatus(function(response) {
     if (response.status === 'connected') {
+
       // Logged into CRUDbrain and Facebook.
-      return {
+      fbUser = {
         id: response.authResponse.userID,
         access_token: response.authResponse.accessToken
-      }
-    } else { 
-      return null;      
+      };
+
     }
   });
 }
