fb69b1017d973d4e44bd3283d58ef21957801ea6
pete & rich <rich.churcher@gmail.com>
2015-06-09 09:38:51 +1200

    Change IDs

--- js/splashMain.js
+++ js/splashMain.js
@@ -23,13 +23,14 @@ $(document).ready(function(){
 });
 
 
-
 function facebookSdk() {
+
 	window.fbAsyncInit = function() {
 	  FB.init({
-	    appId      : '1642565209312684',
-	    cookie     : true,  // session cookies
-	    xfbml      : true,  // parse social plugins on this page
+      //appId      : '1642565209312684',  // Inkling (PRODUCTION)
+      appId      : '1643906175845254',    // CRUDbrain (test)
+	    cookie     : true, 
+	    xfbml      : true,                  // parse social plugins on this page
 	    version    : 'v2.2'
 	  });
 
@@ -48,6 +49,7 @@ function facebookSdk() {
 	  var js, fjs = d.getElementsByTagName(s)[0];
 	  if (d.getElementById(id)) return;
 	  js = d.createElement(s); js.id = id;
+		//js.src = "//connect.facebook.net/en_US/sdk.js";     // PRODUCTION
 	  js.src = "http://connect.facebook.net/en_US/sdk.js";
 	  fjs.parentNode.insertBefore(js, fjs);
 	}(document, 'script', 'facebook-jssdk'));
