ff5d4b320669b79b514c7bab0f965ff97d3fe1e6
peterjacobson <peterjacobson.nz@gmail.com>
2015-06-09 15:12:17 +1200

    animated click handler, respawns, hides & launches text bubble

--- js/bubbleView.js
+++ js/bubbleView.js
@@ -59,30 +59,83 @@ function renderBubble(bubble) {
 }
 
 function renderInputOptions(e) {
-  console.log($('#inputOptionBox'));
-
-  if ($('#inputOptionBox')) {
+  if ($('#inputOptionBox').length === 0) {
     var $inputOptionBox = $('<div id="inputOptionBox"></div>');
     var $textOption = $('<i id="text" class="fa fa-align-left"></i>');
     var $photoOption = $('<i id="photo" class="fa fa-camera"></i>');
     var $audioOption = $('<i id="audio" class="fa fa-microphone"></i>')
     var $videoOption = $('<i id="video" class="fa fa-video-camera"></i>')
     var $drawOption = $('<i id="draw" class="fa fa-pencil"></i>')
-    
-    $textOption.click(function(e){createBubble(e)})
-    // $photoOption
-
     $inputOptionBox.append($textOption);
     $inputOptionBox.append($photoOption);
     $inputOptionBox.append($audioOption);
     $inputOptionBox.append($videoOption);
     $inputOptionBox.append($drawOption);
+    $('#board').append($inputOptionBox);
+  } else {
+    var $inputOptionBox = $('#inputOptionBox');
+    var $textOption = $('#text');
+    var $photoOption = $('#photo');
+    var $audioOption = $('#audio')
+    var $videoOption = $('#video')
+    var $drawOption = $('#draw')
   }
+
+  $inputOptionBox.show()
+  $textOption.on('click', function(e){
+    e.stopImmediatePropagation()
+    $inputOptionBox.hide()
+    createBubble(e)
+  });
+
   var xPos = e.pageY + 'px';
   var yPos = e.pageX + 'px';
-  $inputOptionBox.css({
+  $('#inputOptionBox').css({
     'top': xPos,
     'left': yPos
   })
-  $('#board').append($inputOptionBox);
+  var options = [
+    $textOption,
+    $photoOption,
+    $audioOption,
+    $videoOption,
+    $drawOption
+  ]
+  animateOptions(options);
+}
+
+function animateOptions(options) {
+  console.log('options in animateOptions: ', options);
+  resetOptionPosition(options)
+  var iconSize = 16 // px
+  // http://www.mathopenref.com/coordpolycalc.html for cartesian polygon coords below
+  var finalCentrePositions = [
+    [0,   -20],
+    [19,  -6 ],
+    [12,  16 ],
+    [-12, 16 ],
+    [-19, -6 ]
+  ]
+  var finalTopLeftCoords = finalCentrePositions.map(function(el) {
+    return el.map(function(coord) {
+      return coord - iconSize/2
+    });
+  });
+  for (var i = 0; i < finalTopLeftCoords.length; i++) {
+    options[i].animate({
+      'left': finalTopLeftCoords[i][1],
+      'top': finalTopLeftCoords[i][0]
+    }, 400)
+  }
+  console.log(finalTopLeftCoords)
+}
+
+function resetOptionPosition(options) {
+  console.log(options)
+  for (var i = 0; i < options.length; i++) {
+    options[i].css({
+      'top': 0,
+      'left': 0
+    })
+  };
 }
