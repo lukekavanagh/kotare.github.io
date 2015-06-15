3f7d05427d1bcc4f1936564d1bd27d3330f875a0
Rich Churcher <rich.churcher@gmail.com>
2015-06-15 17:46:51 +1200

    Test Heroku SSL

--- js/net/apiFacade.js
+++ js/net/apiFacade.js
@@ -7,7 +7,8 @@ var ApiFacade = (function() {
   //var BACKEND_BASE_URI = 'http://crudbrain.herokuapp.com/api/v1';
 
   // PRODUCTION
-  var BACKEND_BASE_URI = 'https://crudbrain.inkling.space/api/v1';
+  //var BACKEND_BASE_URI = 'https://crudbrain.inkling.space/api/v1';
+  var BACKEND_BASE_URI = 'https://crudbrain.herokuapp.com/api/v1';
 
   function getBoard (boardId, callback) {
     $.ajax({
