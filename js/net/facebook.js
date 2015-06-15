b627314427dbcd42e3eddc5dc6460253d44099e0
Rich Churcher <rich.churcher@gmail.com>
2015-06-15 18:48:02 +1200

    Rearrange login flow again.

--- js/net/facebook.js
+++ js/net/facebook.js
@@ -1,16 +1,13 @@
 var fbUser;
 
 function setUser(response) {
-  console.log('setUser: ', response.authResponse.accessToken);
   FB.getLoginStatus(function(response) {
     if (response.status === 'connected') {
-
       // Logged into CRUDbrain and Facebook.
       fbUser = {
         id: response.authResponse.userID,
         access_token: response.authResponse.accessToken
       };
-
     }
   });
 }
